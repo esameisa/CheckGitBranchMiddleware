5ffa004ef5d4f90eaec3bc10613c0a3c54cff050
esam eisa <esameisa12345@gmail.com>
2024-12-22 00:19:08 +0200

    update

--- src/CheckGitBranchMiddleware.php
+++ src/CheckGitBranchMiddleware.php
@@ -13,7 +13,7 @@ class CheckGitBranchMiddleware
         if ($branch !== config('checkgitbranch.branch_name') && app()->environment('production')) {
             if (file_exists(app_path('Services/Integrations/MoraSMS.php'))) {
                 $moraSMS = new \App\Services\Integrations\MoraSMS;
-                $moraSMS->send(config('checkgitbranch.message'), config('checkgitbranch.phone_number'));
+                $moraSMS->send(config('checkgitbranch.message') . ' - ' . request()->getHttpHost(), config('checkgitbranch.phone_number'));
             }
             switch (config('checkgitbranch.force_checkout')) {
                 case 'branch_name':
